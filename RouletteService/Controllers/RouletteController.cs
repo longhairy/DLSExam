2932089918ac463ee4798b4062288c49f3f9036a
longhairy <londo112@hotmail.com>
2023-12-28 14:47:58 +0100

    roulette

--- RouletteService/Controllers/RouletteController.cs
+++ RouletteService/Controllers/RouletteController.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
+using SharedModels;
 using System.Data;
 
 namespace RouletteService.Controllers
@@ -16,20 +17,66 @@ namespace RouletteService.Controllers
             if (!bet_type_tables.Any())
             {
                 rouletteDBConnection.Execute("CREATE TABLE bet_type (" +
-                    "id INT NOT NULL AUTO_INCREMENT," +
+                    "bet_type_id INT NOT NULL AUTO_INCREMENT," +
                     "name VARCHAR(50) NOT NULL, " +
                     "multiplier DECIMAL(2,2) NOT NULL, " +
                     "max_bet Decimal(10,2) NOT NULL, " +
                     "min_bet Decimal(10,2) NOT NULL, " +
-                    "PRIMARY KEY (id))");
+                    "PRIMARY KEY (bet_type_id))");
+
+                rouletteDBConnection.Execute("INSERT INTO " +
+                    "bet_type (name,multiplier,max_bet,min_bet)" +
+                    "values ('Even',2,100,1)");
+                rouletteDBConnection.Execute("INSERT INTO " +
+                    "bet_type (name,multiplier,max_bet,min_bet)" +
+                    "values ('Odd',2,100,1)");
+                rouletteDBConnection.Execute("INSERT INTO " +
+                    "bet_type (name,multiplier,max_bet,min_bet)" +
+                    "values ('High',2,100,1)");
+                rouletteDBConnection.Execute("INSERT INTO " +
+                    "bet_type (name,multiplier,max_bet,min_bet)" +
+                    "values ('Low',2,100,1)");
+                rouletteDBConnection.Execute("INSERT INTO " +
+                    "bet_type (name,multiplier,max_bet,min_bet)" +
+                    "values ('Exact Number',36,10,1)");
+
             }
-            //comment
+            var roulette_tables = rouletteDBConnection.Query<string>("SHOW TABLES LIKE 'roulette_game'");
+            if (!roulette_tables.Any())
+            {
+                rouletteDBConnection.Execute("CREATE TABLE roulette_game (" +
+                    "gid INT NOT NULL AUTO_INCREMENT," +
+                    "uid INT NOT NULL, " +
+                    "date DATETIME NOT NULL, " +
+                    "bet_type_id INT NOT NULL, " +
+                    "Bet_amount Decimal(10,2) NOT NULL, " +
+                    "result Decimal(10,2) NOT NULL, " +
+                    "PRIMARY KEY (gid)," +
+                    "FOREIGN KEY (bet_type_id) REFERENCES bet_type(bet_type_id))");
+            }
+
         }
-        //[HttpPost("/post/user")]
-        //public void SaveUser([FromQuery] long inputone, [FromQuery] long inputtwo, [FromQuery] long output)
-        //{
-        //    // TODO: update parameters 
-        //    userDBConnection.ExecuteAsync("REPLACE INTO users (inputone, inputtwo, output, operation) VALUES (@inputone, @inputtwo, @output, 'addition')", new { inputone = inputone, inputtwo = inputtwo, output = output, operation = "addition" });
-        //}
+        [HttpGet("/get/bet_type")]
+        public IActionResult GetBetTypes()
+        {
+            // Retrieve the user's bet history from the database
+            var betTypes = rouletteDBConnection.Query<RouletteGame>("SELECT * FROM roulette_game");
+
+            // Return the bet history as JSON (you can customize this based on your needs)
+            return Json(betTypes);
+            
+        }
+        [HttpPost("/Post/bet")]
+        public double PostBet([FromQuery] int uid, [FromQuery] int bet_type, [FromQuery] double bet_amount, [FromQuery] int bet_number)
+        {
+            RouletteGame game = new RouletteGame(uid, bet_type, bet_amount,bet_number);
+            game.Spin();
+            // Retrieve the user's bet history from the database
+            var betTypes = rouletteDBConnection.Query<RouletteGame>("SELECT * FROM roulette_game");
+
+            // Return the bet history as JSON (you can customize this based on your needs)
+            return game.result;
+
+        }
     }
 }
---
+++ SharedModels/RouletteGame.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SharedModels
+{
+    public class RouletteGame
+    {
+
+        public RouletteGame(int uid, int bet_type, double bet_amount, int bet_number)
+        {
+            this.uid = uid;
+            this.type = new(bet_type);
+            this.bet_amount = bet_amount;
+            this.bet_number = bet_number;
+        }
+
+        private int gid { get; set; }
+        private int uid { get; set; }
+        private DateTime date { get; set; }
+        private BetType type { get; set; }
+        private double bet_amount { get; set; }
+        private int bet_number { get; set; }
+        public double result { get; set; }
+
+        public void Spin()
+        {
+            Random rand = new Random();
+            int spinresult = rand.Next(1,37);
+            bool isWinning = false;
+            switch(type.id)
+            {
+                case 1:
+                    if(spinresult%0==0 && bet_amount >= type.min_bet && bet_amount <= type.max_bet)
+                        isWinning = true;
+                    break;
+                case 2:
+                    if (spinresult % 0 == 1 && bet_amount >= type.min_bet && bet_amount <= type.max_bet)
+                        isWinning = true;
+                    break;
+                case 3:
+                    if(spinresult >=19 && bet_amount >= type.min_bet && bet_amount <= type.max_bet)
+                        isWinning = true;
+                    break;
+                case 4:
+                    if (spinresult <= 18 && bet_amount >= type.min_bet && bet_amount <= type.max_bet)
+                        isWinning = true;
+                    break;
+                case 5:
+                    if(spinresult == bet_number && bet_amount >= type.min_bet && bet_amount <= type.max_bet)
+                        isWinning = true;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            if(isWinning)
+            {
+                result = bet_amount * type.multiplier;
+            }
+        }
+        internal class BetType
+        {
+            public BetType(int id) 
+            {
+                this.id = id;
+                switch (id)
+                {
+                    case 1:
+                        name = "Even";
+                        multiplier = 2;
+                        max_bet = 100;
+                        min_bet = 1;
+                        break;
+                    case 2:
+
+                        name = "Odd";
+                        multiplier = 2;
+                        max_bet = 100;
+                        min_bet = 1;
+                        break;
+                    case 3:
+
+                        name = "High";
+                        multiplier = 2;
+                        max_bet = 100;
+                        min_bet = 1;
+                        break;
+                    case 4:
+
+                        name = "Low";
+                        multiplier = 2;
+                        max_bet = 100;
+                        min_bet = 1;
+                        break;
+                    case 5:
+
+                        name = "Exact Number";
+                        multiplier = 36;
+                        max_bet = 10;
+                        min_bet = 1;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+
+                }
+            }
+            public int id { get; set; }
+            public string name { get; set; }
+            public double multiplier { get; set; }
+            public double max_bet { get; set; }
+            public double min_bet { get; set;}
+
+        }
+    }
+}
